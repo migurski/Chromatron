0838d25fdeecf405b9619faec424f48baa31dfe7
Michal Migurski <mike@teczno.com>
2017-07-25 22:31:17 -0700

    Added a primitive selection component

--- src/index.js
+++ src/index.js
@@ -9,11 +9,48 @@ function Color(props)
 {
     return (
         <circle
-            onClick={() => props.field.onClick(props.color)}
+            onClick={() => props.field.onClickedColor(props.color)}
             cx={props.color.x} cy={props.color.y} r={props.color.r}
             style={{fill: props.color.fill.hex()}}
             />
         )
+}
+
+class Control extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+
+        this.field = props.field;
+        this.state = {
+            color: props.color,
+            handle: {x: props.color.x + props.color.r, y: props.color.y}
+            };
+    }
+
+    render()
+    {
+        var color = this.state.color,
+            handle = this.state.handle;
+    
+        return (
+            <g>
+            <circle
+                cx={color.x} cy={color.y} r={color.r}
+                style={{pointerEvents: 'none', fill: 'transparent', mixBlendMode: 'difference', stroke: 'white', strokeWidth: 3, strokeDasharray: '7 4'}}
+                />
+            <circle
+                cx={color.x} cy={color.y} r="5"
+                style={{fill: 'white', stroke: 'black', strokeWidth: 2}}
+                />
+            <circle
+                cx={handle.x} cy={handle.y} r="5"
+                style={{fill: 'white', stroke: 'black', strokeWidth: 2}}
+                />
+            </g>
+            )
+    }
 }
 
 class Form extends React.Component
@@ -63,23 +100,50 @@ class Field extends React.Component
                 {fill: Chroma('#f0f'), x: 160, y: 160, r: 80},
                 {fill: Chroma('#ff0'), x: 120, y: 120, r: 100}
             ],
-            active: [ ],
+            active: null,
+            stack: [ ],
             index: 0
         };
+
+        this.onClickedBackground = this.onClickedBackground.bind(this);
     }
     
-    onClick(color)
+    onClickedBackground()
     {
-        this.state.active.unshift(color);
-        this.state.active.splice(3);
-        this.setState(this.state);
+        var state = {
+            colors: this.state.colors.slice(),
+            active: null,
+            stack: this.state.stack.slice(),
+            index: this.state.index
+        };
+
+        this.setState(state);
+    }
+    
+    onClickedColor(color)
+    {
+        var state = {
+            colors: this.state.colors.slice(),
+            active: color,
+            stack: this.state.stack.slice(),
+            index: this.state.index
+        };
+        
+        if(color !== state.stack[0])
+        {
+            state.stack.unshift(color);
+            state.stack.splice(3);
+        }
+
+        this.setState(state);
     }
     
     updateColor(color)
     {
         var state = {
             colors: this.state.colors.slice(),
-            active: this.state.active.slice(),
+            active: this.state.active,
+            stack: this.state.stack.slice(),
             index: this.state.index
         };
         
@@ -97,8 +161,10 @@ class Field extends React.Component
 
     render()
     {
-        var colors = this.state.colors.slice();
-
+        var colors = this.state.colors.slice(),
+            forms = [],
+            control = null;
+    
         // smallest circles should appear in front
         colors.sort(function(c1, c2) { return c2.r - c1.r });
 
@@ -108,17 +174,25 @@ class Field extends React.Component
             colors[i] = <Color key={i} field={this} color={colors[i]} />
         }
         
-        var forms = [];
-    
-        for(var j = 0; j < this.state.active.length; j++)
+        // render a stack of forms
+        for(var j = 0; j < this.state.stack.length; j++)
         {
-            forms.push(<Form key={this.state.index++} field={this} color={this.state.active[j]} />)
+            forms.push(<Form key={'form-' + this.state.index++} field={this} color={this.state.stack[j]} />)
+        }
+        
+        // show control for the active color
+        if(this.state.active)
+        {
+            var key = 'control-' + this.state.colors.indexOf(this.state.active);
+            control = <Control key={key} field={this} color={this.state.active} />;
         }
         
         return (
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="650" height="400" style={{backgroundColor: '#f90'}}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="650" height="400">
+                <rect width="100%" height="100%" onClick={this.onClickedBackground} style={{fill: '#f90'}} />
                 {colors}
+                {control}
               </svg>
               <ol>
                 {forms}
